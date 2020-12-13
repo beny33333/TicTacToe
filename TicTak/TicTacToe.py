682f799633f085f34ba90f28bee3b581490d44d4
Beniamin Jarczyk <beniamin.jarczyk@gmail.com>
2020-12-13 03:08:17 +0100

    to był błąd

--- TicTak/TicTacToe.py
+++ TicTak/TicTacToe.py
@@ -1,6 +1,8 @@
 A = ['.','.','.']
 B = ['.','.','.']
 C = ['.','.','.']
+
+ruchy = 9
 result_list = []
 LISTA = [A,B,C]
 list_to_dictionary = ['A1','A2','A3','B1','B2','B3','C1','C2','C3']
@@ -14,14 +16,16 @@ win_result = [[A[0], A[1], A[2]] == ['X','X','X'],
     [A[2], B[1], C[0]] == ['X','X','X']]
 
 def init_board():
-    print(LISTA)
+    print_board()
+
+    
 
 
-def get_move():
+def get_move(ruchy):
     while True:
         guess = str(input('Please dawaj koordynaty gościu!')).upper()
         if guess in list_to_dictionary and guess == 'A1' and guess not in result_list:
-            A[0] = 'X'
+            A[0] = 'X' 
         if guess in list_to_dictionary and guess == 'A2' and guess not in result_list:
             A[1] = 'X'
         if guess in list_to_dictionary and guess == 'A3' and guess not in result_list:
@@ -42,8 +46,9 @@ def get_move():
             print('NT dude, try again')
         if guess not in result_list and guess in list_to_dictionary:
             result_list.append(guess)
-        print(LISTA)
-        return result_list
+            ruchy -= 1
+        print_board()
+        # return ruchy
 
 
 
@@ -62,18 +67,23 @@ def has_won():
 
 
 
-def is_full():
-    if '.' in LISTA:
-        return False
-    else:
+def is_full(ruchy):
+    if ruchy == 0:
         return True
+    else:
+        return False
 
-init_board()
-get_move()
-has_won()
-is_full()
 
-# def print_board():
+
+
+def print_board():
+    
+    print(
+        '    1     2     3  \n A '  , A[0], ' | ',A[1],' | ',A[2],'\n''  -----+-----+------\n B ',B[0],' | ',B[1],' | ',B[2],'\n''  -----+-----+------\n C ',C[0],' | ',C[1],' | ',C[2],' ')
+    
+    
+
+
 
 # def print_result():
 
@@ -85,3 +95,9 @@ is_full()
 
 # if name == "tictactoe_game":
 #     tictactoe_game()
+
+
+init_board()
+get_move(ruchy)
+has_won()
+is_full(ruchy)
