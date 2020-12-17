811c846b7b8449464d6bab1df5184b0a43a4ee48
Beniamin Jarczyk <beniamin.jarczyk@gmail.com>
2020-12-18 00:47:33 +0100

    Trzecia zmiana

--- TicTak/TicTacToe2.py
+++ TicTak/TicTacToe2.py
@@ -1,13 +1,35 @@
+
+
+
 A = ['.','.','.']
 B = ['.','.','.']
 C = ['.','.','.']
 LISTA = [A,B,C]
 
+# def lista():
+#     global A
+#     global B
+#     global C
+#     global LIST
+#     A = ['.','.','.']
+#     B = ['.','.','.']
+#     C = ['.','.','.']
+#     LIST = [A,B,C] 
+
 def init_board():
-    if ('X' or '0') in (A or B or C):
-        pass
-    if not any('X' or '0') in (A or B or C):
+    if A == ['.','.','.'] and B == ['.','.','.'] and C == ['.','.','.']:
         print_board()
+    
+   
+    # if A == ['.','.','.'] and B == ['.','.','.'] and C == ['.','.','.']:
+    #     print_board()
+    # if (A != ['.','.','.'] or B != ['.','.','.'] or C != ['.','.','.']) and any(win_result) is False:
+    #     print_board()
+    # # if (A != ['.','.','.'] or B != ['.','.','.'] or C != ['.','.','.']) and any(win_result) is True:
+    # #     A[0] = '.' and A[1] = '.' and A[2] = '.' and B[0] = '.' and B[1] = '.' and B[2] = '.' and C[0] = '.' and C[1] = '.' and C[2] = '.'
+    # #     print_board()
+    # return A,B,C
+
 
 def get_move():
     global literaki
@@ -15,19 +37,19 @@ def get_move():
     global coordinates_list
     coordinates_list = ['A1','A2','A3','B1','B2','B3','C1','C2','C3']
     used_coordinates = []
+    
     literaki = str(input("Wprowadź koordynaty:"))
     
     while literaki.upper() in coordinates_list and literaki.upper() not in used_coordinates:
         used_coordinates.append(literaki)
-    
+    return literaki
 
 def mark():
-    
     # D = ['X','0','X','0','X','0','X','0','X']
     # PlayerX = D[0], D[2], D[4], D[6], D[8]
     # Player0 = D[1], D[3], D[5], D[7]
     # print(PlayerX)
-    mark_istead_of_dot = 'X'
+    mark_istead_of_dot = ('X' or '0')
     if literaki == 'A1' and A[0] == '.':
         A[0] = mark_istead_of_dot
     if literaki == 'A2' and A[1] == '.':
@@ -90,44 +112,45 @@ def print_board():
          '  -----+-----+------\n C '
          ,C[0],' | ',C[1],' | ',C[2],' ')
 
+
 def print_result():
     if any(win_result[0:8]) == True:
+        print_board()
         print("Wygrał gracz X!")
-        main()
+        main_menu()
     if any(win_result[8:]) == True:
+        print_board()
         print("wygrał gracz O") 
-        main()
+        main_menu()
     if win_result is False and not '.' in A and not '.' in B and not '.' in C:
+        print_board()
         print("It's tie")
-        main()
+        main_menu()
     else:
+        print_board()
         main()
     
 def main():
-    LISTA = init_board()
-    if ('X' or '0') in (A or B or C):
-        get_move()
-        mark()
-        has_won()
-        is_full()
-        print_result()
-    if not any('X' or '0') in (A or B or C):
-        Tryb_gry = input('Jeśli chcesz zagrać sam ze sobą to se graj i napisz yes or no : ''\n')
-        get_move()
-        mark()
-        has_won()
-        is_full()
-        print_result()
-    else:
-        print('napisz jes bambaryło jedna!')
-    while input('Chcesz se zagrać jeszcze raz to napisz jes').upper() == ('yes').upper():
+    # LISTA = [A,B,C]
+    init_board()
+    # if ('X' or '0') in (A or B or C):
+    get_move()
+    mark()
+    has_won()
+    is_full()
+    print_result()
+
+def main_menu():
+    Tryb_gry = input('Jeśli chcesz zagrać sam ze sobą to se graj i napisz yes or no : ''\n')
+    if Tryb_gry == 'yes':
+        # A[0] == '.' and A[1] == '.' and A[2] == '.' and B[0] == '.' and B[1] == '.' and B[2] == '.' and C[0] == '.' and C[1] == '.' and C[2] == '.'
         main()
-        
+    # else:
+    #     main_menu()
 
 
 if __name__ == "__main__":
-    print('Witamy w symulatorze grilla w mózgu''\n')
-    main()
+    main_menu()
 
 
 
